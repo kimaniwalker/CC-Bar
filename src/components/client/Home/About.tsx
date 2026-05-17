ec05ff8d1ab4c4b8601f7a1d0cd4397cd68273c1
walkerkimani <kimani.walker@shipt.com>
2026-05-17 10:30:02 -0500

    update image

--- src/components/client/Home/About.tsx
+++ src/components/client/Home/About.tsx
@@ -5,7 +5,7 @@ import { motion } from "motion/react";
 import { Stack } from "@/components/ds/Stack";
 import { useRouter } from 'next/navigation';
 export default function About() {
-  
+
   const router = useRouter();
   const getTodayCST = () => {
     return new Intl.DateTimeFormat("en-CA", {
@@ -21,68 +21,68 @@ export default function About() {
 
   return (
     <div className="h-screen relative overflow-hidden">
-      <div className="absolute inset-0 bg-cover bg-center bg-fixed bg-[url('/wavy.png')] bg-blend-darken"></div>
+      <div className="absolute inset-0 bg-cover bg-center bg-fixed bg-[url('/wave.svg')] bg-blend-darken"></div>
       <div className="relative z-10 flex flex-col h-full">
         <Stack justify="center">
-                <Text as="div" size="xxl" className="text-center text-white z-10 absolute top-25 xl:text-9xl lg:text-7xl md:text-5xl text-4xl">
-                  <motion.h2
-                    initial={{
-                      opacity: 0,
-                      y: -25,
-                    }}
-                    whileInView={{
-                      scale: [0.75, 1],
-                      opacity: 1,
-                      y: 0,
-                    }}
-                    transition={{
-                      ease: "easeInOut",
-                      duration: 2.5,
-                      delay: 0.5,
-                    }}
-                    viewport={{ once: true, amount: 0.5 }}
-                  >
-                    CANDLE COW BAR
-                  </motion.h2>
-                </Text>
-              </Stack>
-              <Stack
-                align="center"
-                direction="col"
-                gap="lg"
-                className="mt-8 mb-8 w-full absolute bottom-50"
+          <Text as="div" size="xxl" className="text-center text-white z-10 absolute top-25 xl:text-9xl lg:text-7xl md:text-5xl text-4xl">
+            <motion.h2
+              initial={{
+                opacity: 0,
+                y: -25,
+              }}
+              whileInView={{
+                scale: [0.75, 1],
+                opacity: 1,
+                y: 0,
+              }}
+              transition={{
+                ease: "easeInOut",
+                duration: 2.5,
+                delay: 0.5,
+              }}
+              viewport={{ once: true, amount: 0.5 }}
+            >
+              CANDLE COW BAR
+            </motion.h2>
+          </Text>
+        </Stack>
+        <Stack
+          align="center"
+          direction="col"
+          gap="lg"
+          className="mt-8 mb-8 w-full absolute bottom-50"
+        >
+          <div className="sm:max-w-auto md:max-w-256 p-4">
+            <Text as="div" size="lg">
+              <motion.h3
+                initial={{
+                  y: 50,
+                  opacity: 0,
+                }}
+                whileInView={{
+                  opacity: 1,
+                  y: 0,
+                }}
+                transition={{
+                  ease: "easeInOut",
+                  duration: 2.5,
+                  delay: 1,
+                }}
+                viewport={{ once: true, amount: 0.1 }}
               >
-                <div className="sm:max-w-auto md:max-w-256 p-4">
-                  <Text as="div" size="lg">
-                    <motion.h3
-                      initial={{
-                        y: 50,
-                        opacity: 0,
-                      }}
-                      whileInView={{
-                        opacity: 1,
-                        y: 0,
-                      }}
-                      transition={{
-                        ease: "easeInOut",
-                        duration: 2.5,
-                        delay: 1,
-                      }}
-                      viewport={{ once: true, amount: 0.1 }}
-                    >
-                     
-        
-                      <div className="mt-12 w-full flex justify-center">
-                        <button onClick={handleBookClick} className="px-8 py-4 bg-black text-white rounded-4xl hover:bg-gray-800 transition z-10">
-                          <Text as="span" size="lg" className="text-3xl">Book Your Experience</Text>
-                        </button>
-                      </div>
-        
-        
-                    </motion.h3>
-                  </Text>
+
+
+                <div className="mt-12 w-full flex justify-center">
+                  <button onClick={handleBookClick} className="px-8 py-4 bg-black text-white rounded-4xl hover:bg-gray-800 transition z-10">
+                    <Text as="span" size="lg" className="text-3xl">Book Your Experience</Text>
+                  </button>
                 </div>
-              </Stack>
+
+
+              </motion.h3>
+            </Text>
+          </div>
+        </Stack>
       </div>
     </div>
   );
--- src/components/client/Home/BannerAd.tsx
+++ src/components/client/Home/BannerAd.tsx
@@ -5,13 +5,12 @@ import React from "react";
 export default function BannerAd({ rotate }: { rotate?: boolean }) {
   return (
     <Stack
-      className={`relative w-full lg:h-192 md:h-120 h-80 ${
-        rotate && "rotate-180"
-      }`}
+      className={`relative w-full lg:h-192 md:h-120 h-80 ${rotate && "rotate-180"
+        }`}
     >
       <Image
         priority
-        src="/wavy.png"
+        src="/wave.svg"
         alt="image"
         fill
         style={{ objectFit: "cover" }}
