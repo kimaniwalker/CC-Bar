7cec2ef2486d3fb4e3963cd61c69d603bed4eb8d
walkerkimani <kimani.walker@shipt.com>
2026-05-17 01:33:16 -0500

    update env

---
+++ src/app/not-found/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-6xl font-bold mb-4">404</h1> <span className="text-xl text-gray-600">Page Not Found</span>
+        </div>
+    )
+}
