cae6b02f09788cf58bb9074014912d29751e9d8f
walkerkimani <kimani.walker@shipt.com>
2026-05-28 18:43:23 -0500

    wrap app in suspense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { CartModalProvider } from "@/components/client/CartModalContext";
 import { AuthProvider } from "@/components/client/Auth/AuthContext";
 import { FavoritesProvider } from "@/components/client/Favorites/FavoritesContext";
 import { Toaster } from "sonner";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -21,27 +22,29 @@ export default function RootLayout({
   return (
     <html>
       <body>
-        <AuthProvider>
-          <CartModalProvider>
-            <CartProvider>
-              <ModalProvider>
-                <FavoritesProvider>
-                  <Header />
-                  {children}
-                  <Toaster
-                    position="top-right"
-                    richColors
-                    closeButton
-                    expand
-                    visibleToasts={3}
-                  />
-                </FavoritesProvider>
-              </ModalProvider>
-            </CartProvider>
-          </CartModalProvider>
-        </AuthProvider>
-        <div id="modal-root"></div>
-        <div id="cart-modal-root"></div>
+        <Suspense>
+          <AuthProvider>
+            <CartModalProvider>
+              <CartProvider>
+                <ModalProvider>
+                  <FavoritesProvider>
+                    <Header />
+                    {children}
+                    <Toaster
+                      position="top-right"
+                      richColors
+                      closeButton
+                      expand
+                      visibleToasts={3}
+                    />
+                  </FavoritesProvider>
+                </ModalProvider>
+              </CartProvider>
+            </CartModalProvider>
+          </AuthProvider>
+          <div id="modal-root"></div>
+          <div id="cart-modal-root"></div>
+        </Suspense>
       </body>
     </html>
   );
