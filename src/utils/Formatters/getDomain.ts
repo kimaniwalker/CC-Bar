2241e2ad53f950a42fd2db5a006604b321985e0a
walkerkimani <kimani.walker@shipt.com>
2026-07-08 00:02:03 -0500

    update getDomain

--- src/utils/Formatters/getDomain.ts
+++ src/utils/Formatters/getDomain.ts
@@ -2,7 +2,8 @@ export const getDomain = () => {
   if (process.env.VERCEL_ENV === "production") {
     return process.env.NEXT_PUBLIC_DOMAIN;
   }
-  return process.env.NEXT_PUBLIC_VERCEL_URL || process.env.VERCEL_URL
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL ?? process.env.VERCEL_URL}`
-    : "http://localhost:3000";
+
+  const vercelUrl =
+    process.env.NEXT_PUBLIC_VERCEL_URL || process.env.VERCEL_URL;
+  return vercelUrl ? `https://${vercelUrl}` : "http://localhost:3000";
 };
