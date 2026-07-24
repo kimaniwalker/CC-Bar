a0c5f9de3a105cdf8ee4415f42a041897f01a23e
walkerkimani <kimani.walker@shipt.com>
2026-07-24 01:11:28 -0500

    update next config

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,16 @@ const nextConfig: NextConfig = {
     ],
   },
   cacheComponents: true,
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "candlecowbar.com" }],
+        destination: "https://www.candlecowbar.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
